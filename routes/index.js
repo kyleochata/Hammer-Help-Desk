ecc78b5ed6ccc46d21efe215f800f46102e5a0bc
kyleochata <kyleochata@gmail.com>
2023-10-03 13:47:06 -0700

    server, helpers, model/index, routes/index, routes/api/index done

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+// const handlebarsRoutes = require('./handlebarsRoutes');
+// const apiRoutes = require('./apiRoutes');
+
+// router.use('/', handlebarsRoutes);
+// router.use('/api', apiRoutes)
+
+module.exports = router;
