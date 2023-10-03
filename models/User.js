3444457ba2f1c959f7aa86d6a117ca7260b99b36
Kyle Oros <kyleblissoros@gmail.com>
2023-10-03 14:00:17 -0700

    pushing user.js

---
+++ models/User.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require ('brypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6],
+            },
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        role: {
+            type: DataTypes.ENUM('tech', 'client'),
+            allowNull: false,
+            // UNSOLVED: Set default to client.
+            
+        },
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelname: 'user',
+
+    }
+)
