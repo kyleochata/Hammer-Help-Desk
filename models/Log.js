5c91d9276704e6f2aa87a8e80125fcca29d80031
James Brainard <james.brainard2@aol.com>
2023-10-03 13:55:15 -0700

    created Log and ticket model, ticket includes both after create and update

---
+++ models/Log.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+class Log extends Model { };
+
+Log.init(
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      }
+    },
+    ticketId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'ticket',
+        key: 'id',
+      }
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.ENUM('Created', 'Modified', 'Message'),
+      allowNull: false,
+      defaultValue: 'Message',
+    },
+    isHidden: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: 'false',
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'log'
+  }
+);
+
+module.exports = Log;
