25097b7354d3e02819d7b148d930e31a074fcbec
James Brainard <james.brainard2@aol.com>
2023-10-04 20:02:49 -0700

    working on handlebarRoutes, not finisihed yet.

--- routes/apiRoutes/logRoutes.js
+++ routes/apiRoutes/logRoutes.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const logController = require('../../controllers/logControllers');
 
 // POST will call the createLog controller.
-router.route('/api/log/:ticketId?drawer=BOOLEAN')
+router.route('/api/log/:ticketId?')
   .post(logController.createLog);
 
 // PUT will call the editLog controller.
 // DELETE will call the deleteLog controller.
 
-router.route('/api/log/:ticketId/:logId?drawer=BOOLEAN')
+router.route('/api/log/:ticketId/:logId?')
   .put(logController.editLog)
   .delete(logController.deleteLog);
 
